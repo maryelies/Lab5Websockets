bf0da250b7156ab4874b1f47c37ad0b5f394c27b
Margot Desmet <margotdesmet@hotmail.com>
2018-03-28 17:47:50 +0200

    added counter when clicking on question

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -12,12 +12,18 @@ var primus = Primus.connect(url, {
     var pollQuestion = document.querySelector(".pollQuestion");
     var pollAnswer1 = document.querySelector(".pollAnswer1");
     var pollAnswer2 = document.querySelector(".pollAnswer2");
-    
+    var countone = document.querySelector(".countone");
+    var counttwo = document.querySelector(".counttwo");
+
+    console.log(data.count1);
+
     if(pollQuestion){
         //console.log(data);
         pollQuestion.innerHTML=data.question;
         pollAnswer1.innerHTML=data.answer1;
         pollAnswer2.innerHTML=data.answer2;
+        countone.innerHTML=data.count1;
+        counttwo.innerHTML=data.count2;
     }
   });
 
@@ -28,9 +34,40 @@ if(document.querySelector("#submit")){
     primus.write({ 
         question: document.getElementById("question").value,
         answer1: document.getElementById("answerone").value,
-        answer2: document.getElementById("answertwo").value
+        answer2: document.getElementById("answertwo").value,
+        count1: 0,
+        count2: 0
     });
     e.preventDefault();
   });
 
 }
+
+if(document.querySelector(".pollQuestion")){
+
+    document.querySelector(".pollAnswer1").addEventListener("click", function(e){
+      primus.write({ 
+          count1: parseInt(document.querySelector(".countone").innerHTML)+1,
+          count2: parseInt(document.querySelector(".counttwo").innerHTML),
+          question: document.querySelector(".pollQuestion").innerHTML,
+          answer1: document.querySelector(".pollAnswer1").innerHTML,
+          answer2: document.querySelector(".pollAnswer2").innerHTML
+
+      });
+      e.preventDefault();
+    });
+
+    document.querySelector(".pollAnswer2").addEventListener("click", function(e){
+      primus.write({ 
+          count1: parseInt(document.querySelector(".countone").innerHTML),
+          count2: parseInt(document.querySelector(".counttwo").innerHTML)+1,
+          question: document.querySelector(".pollQuestion").innerHTML,
+          answer1: document.querySelector(".pollAnswer1").innerHTML,
+          answer2: document.querySelector(".pollAnswer2").innerHTML
+
+      });
+      e.preventDefault();
+    });
+}
+
+//console.log(parseInt(document.querySelector(".countone").innerHTML)+1);
